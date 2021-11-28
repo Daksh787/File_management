c216d5309c9f07ca8fd7065597df88efa20deff3
Daksh787 <95178542+Daksh787@users.noreply.github.com>
2021-11-28 20:41:13 +0530

    Add files via upload

---
+++ 20SOECE11040_DAKSH KUMBHANI_PBL_3CEA.py
@@ -0,0 +1,18 @@
+import os,shutil
+
+path = input(r"Enter source path :")
+
+files = os.listdir(path)
+
+for file in files:
+
+        filename,extention = os.path.splitext(file) 
+        extention = extention[1:] 
+
+        if os.path.exists(path +'/'+extention):
+            shutil.move(path+'/'+file,path+'/'+extention+'/'+file)
+            
+        else :
+            os.makedirs(path +'/'+extention)
+            shutil.move(path+'/'+file,path+'/'+extention+'/'+file)
+
